d5d6a6935e648b7752896322dd216cb3fe6d2561
Lubeth <lubethjimenez@yahoo.com>
2026-02-18 23:15:05 -0500

    Add submission data safely

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 'use strict';
 
 const textarea = document.querySelector("#jsonData");
-console.log(document);
+
+
+const input = document.getElementById("input");
+const input2 = document.getElementById("input2");
+const input3 = document.getElementById("input3");
+const input4 = document.getElementById("input4");
+const input5 = document.getElementById("input5");
 
 const jsonFORM = document.getElementById("jsonFORM");
 console.log(jsonFORM);
 
 jsonFORM.addEventListener("render",function(event){
     event.preventDefault();
+    console.log(event)
     console.log(jsonFORM.jsonData.value);
     try {
-        const obj = JSON.parse(jsonFORM.jsonData.value);
-        
+        const items = JSON.parse(jsonFORM.jsonData.value);
+        input.textContent = items[0];
+        input2.textContent = items[2];
+        input3.textContent = items[3];
+        input4.textContent = items[4];
+        input5.textContent = items[5];
+        console.log(items);
     } catch (error) {
-       console.log("Ignore") 
+       input.textContent = "Check your work!"
+       input2.textContent = ""
+       input3.textContent = ""
+       input4.textContent = ""
+       input5.textContent = ""
     }
 })

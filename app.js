d00b115deeba2f3d6b00a0cda59704830f978dc4
Lubeth <lubethjimenez@yahoo.com>
2026-02-18 22:17:22 -0500

    Add listener event w/ no reload

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ console.log(document);
 
 const jsonFORM = document.getElementById("jsonFORM");
 console.log(jsonFORM);
+
+jsonFORM.addEventListener("render",function(event){
+    event.preventDefault();
+    console.log(jsonFORM.jsonData.value);
+})
